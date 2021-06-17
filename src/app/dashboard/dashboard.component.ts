bb686eaa5c83de92e636d89f572a46dabbfd9b8f
Geoff Fox <fox.geoffrey@gmail.com>
2021-06-16 20:35:50 -0700

    add server list

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Server {
+  name: string;
+  content: string;
+
+  constructor(name: string, content: string) { }
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  name: string = '';
+  content: string = '';
+  servers: Server[] = [];
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.servers = [
+      { name:'TestServer', content: 'This is just a name to test'},
+      { name: 'SecondServer', content: 'Another name to test'}
+    ]
+    console.log(this.servers);
+  }
+
+  addServer() {
+    this.servers.push(new Server(this.name, this.content));
   }
 
 }
