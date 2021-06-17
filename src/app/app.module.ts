d196e25899d46eae26078da3664b47ecf7b0b43b
Geoff Fox <fox.geoffrey@gmail.com>
2021-06-16 19:15:10 -0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServerListComponent } from './server-list/server-list.component';
+import { ServerItemComponent } from './server-item/server-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ServerListComponent,
+    ServerItemComponent
   ],
   imports: [
     BrowserModule,
